5152605f40218d1bbfd619cc0fd239a8022e10ac
ghazal <ahmed.ghazal365@gmail.com>
2022-11-09 07:59:54 +0200

    posts features

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = const Color.fromARGB(255, 104, 6, 71);
+final secondaryColor = const Color.fromARGB(255, 18, 71, 56);
+
+final appTheme = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: primaryColor,
+      centerTitle: true,
+    ),
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
+    colorScheme: ColorScheme.light(
+      primary: primaryColor,
+    ),
+    progressIndicatorTheme: ProgressIndicatorThemeData(color: primaryColor),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+      foregroundColor: secondaryColor,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primaryColor),
+        iconColor: secondaryColor,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: secondaryColor),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: primaryColor),
+          borderRadius: BorderRadius.circular(8),
+        )));
